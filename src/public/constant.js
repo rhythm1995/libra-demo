f66fd4f76545346904319727edd8bceb2343e721
张崇 <nuptunee@gmail.com>
2020-01-14 08:39:51 +0800

    feat:添加常量表

---
+++ src/public/constant.js
@@ -0,0 +1,9 @@
+/**
+ * 常量表，多次使用的常量定义在这里，其值与数据库中字段的值保持一致
+ */
+const constant = {
+	// 用户角色类型
+	USER_ROLE_TYPE_SUPERADMIN: 1, // 平台超级管理员
+	USER_ROLE_TYPE_ADMIN: 2, // 普通管理员
+	USER_ROLE_TYPE_NORMAL: 3, // 普通用户
+}
